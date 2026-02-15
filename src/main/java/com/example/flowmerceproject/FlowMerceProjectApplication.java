224a5ac83e2cd878f40612116b053069725ee778
kokom <ka.mahrous@gmail.com>
2026-02-15 22:29:21 +0200

    Init Commit

---
+++ src/main/java/com/example/flowmerceproject/FlowMerceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.flowmerceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowMerceProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowMerceProjectApplication.class, args);
+    }
+
+}
