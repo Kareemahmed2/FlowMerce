05b0342d34f8e18ba08c70c030eb2d119949638e
HanaAbdelbari <hanaabdelbari882@gmail.com>
2026-05-15 21:25:36 +0300

    category entity

---
+++ src/main/java/com/example/flowmerceproject/ProductManagement/entity/Category.java
@@ -0,0 +1,23 @@
+package com.example.flowmerceproject.ProductManagement.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "categories")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Integer categoryId;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+}
