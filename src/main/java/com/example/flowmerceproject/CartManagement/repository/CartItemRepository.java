d7ce7ee0026bf712b0206dbff94cb543953c1feb
HanaAbdelbari <hanaabdelbari882@gmail.com>
2026-05-16 10:55:07 +0300

    make CartItemRepository

--- src/main/java/com/example/flowmerceproject/CartManagement/repository/CartItemRepository.java
+++ src/main/java/com/example/flowmerceproject/CartManagement/repository/CartItemRepository.java
@@ -1,4 +1,21 @@
 package com.example.flowmerceproject.CartManagement.repository;
 
-public class CartItemRepository {
+import com.example.flowmerceproject.CartManagement.entity.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.List;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Integer> {
+
+    List<CartItem> findByCart_CartId(Integer cartId);
+
+    // Check if this product already exists in the cart
+    Optional<CartItem> findByCart_CartIdAndProduct_ProductId(
+            Integer cartId, Integer productId);
+
+    boolean existsByCart_CartIdAndProduct_ProductId(
+            Integer cartId, Integer productId);
 }
