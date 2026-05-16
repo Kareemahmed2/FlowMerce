89a6afe9a0bd131283736bb58d8b69d5726f3b1f
HanaAbdelbari <hanaabdelbari882@gmail.com>
2026-05-16 10:57:50 +0300

    WishlistRepository handle user id associated to his wishlist

--- src/main/java/com/example/flowmerceproject/CartManagement/repository/WishlistRepository.java
+++ src/main/java/com/example/flowmerceproject/CartManagement/repository/WishlistRepository.java
@@ -1,4 +1,23 @@
 package com.example.flowmerceproject.CartManagement.repository;
 
-public class WishlistRepository {
+import com.example.flowmerceproject.CartManagement.entity.Wishlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface WishlistRepository extends JpaRepository<Wishlist, Integer> {
+
+    List<Wishlist> findByUser_UserId(Integer userId);
+
+    Optional<Wishlist> findByUser_UserIdAndProduct_ProductId(
+            Integer userId, Integer productId);
+
+    boolean existsByUser_UserIdAndProduct_ProductId(
+            Integer userId, Integer productId);
+
+    void deleteByUser_UserIdAndProduct_ProductId(
+            Integer userId, Integer productId);
 }
