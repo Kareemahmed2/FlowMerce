eae718d7e3130984351a75703d4d83923f3112f0
HanaAbdelbari <hanaabdelbari882@gmail.com>
2026-05-16 10:52:19 +0300

    make wishlist entity for every user wanted to shop later

--- src/main/java/com/example/flowmerceproject/CartManagement/entity/Wishlist.java
+++ src/main/java/com/example/flowmerceproject/CartManagement/entity/Wishlist.java
@@ -1,4 +1,39 @@
 package com.example.flowmerceproject.CartManagement.entity;
 
+import com.example.flowmerceproject.ProductManagement.entity.Product;
+import com.example.flowmerceproject.UserManagement.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(
+        name = "wishlists",
+        uniqueConstraints = @UniqueConstraint(
+                columnNames = {"user_id", "product_id"} // one product per user in wishlist
+        )
+)
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Builder
 public class Wishlist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "wishlist_id")
+    private Integer wishlistId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
 }
